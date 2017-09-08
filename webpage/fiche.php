92ed31a83f58a1648bb06aa4736bcc12f32a8202
farrradise <marzini.f@gmail.com>
2017-09-08 14:40:11 +0200

    start to create the associative array

---
+++ webpage/fiche.php
@@ -0,0 +1,33 @@
+<?php
+// On crée notre array $fichesproduits
+$fichesproduits = array (
+    '1' => array(
+      'titre' => '',
+      'description_courte' => '',
+      'resume' => '',
+      'prix' => '',
+      'srcImageProduit' = ''
+      ),
+    '2' => array(
+      'titre' => '',
+      'description_courte' => '',
+      'resume' => '',
+      'prix' => '',
+      'srcImageProduit' = ''
+      ),
+    '3' => array(
+      'titre' => '',
+      'description_courte' => '',
+      'resume' => '',
+      'prix' => '',
+      'srcImageProduit' = ''
+      ),
+    '4' => array(
+      'titre' => '',
+      'description_courte' => '',
+      'resume' => '',
+      'prix' => '',
+      'srcImageProduit' = ''
+      )
+    );
+?>
