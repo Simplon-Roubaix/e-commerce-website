22d3d4d595ad1edc4ef45d40a85ee991344abff4
farrradise <marzini.f@gmail.com>
2017-09-08 14:18:37 +0200

    start the header

---
+++ webpage/header.php
@@ -0,0 +1,53 @@
+<?php
+
+// faire l'ouverture de session pour les super variables
+ ?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>e-commerce-website</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="apple-touch-icon" href="apple-touch-icon.png">
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+      <link rel="stylesheet" href="css/main.css">
+      <link rel="stylesheet" href="css/normalize.css">
+    </head>
+
+    <body>
+
+      <!-- beginning of the header  -->
+      <header>
+        <nav>
+          <div class="card-panel grey lighten-1 nav-wrapper white-text">
+            <a href="#!" class="divLogo brand-logo ">
+              <img class="logo" src="img/logo.png" alt="logo entreprise">
+              <span>PhoneHouse</span>
+            </a>
+            <ul class="right hide-on-med-and-down">
+              <li>
+                <a href="#">
+                  <i class="material-icons">search</i>
+                  <!-- //mettre un input pour simuler recherche -->
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="material-icons">person</i>
+                  <!-- //mettre un toggle pour s'enregistrer et sidentifier -->
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i class="material-icons">local_grocery_store</i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
