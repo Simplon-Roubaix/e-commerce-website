e7e4c3105ab6f011ccb2500f38e47cc3df168fcf
farrradise <marzini.f@gmail.com>
2017-09-08 15:08:01 +0200

    started the pseudo code for the for loop on index.php"

--- webpage/fiche.php
+++ webpage/fiche.php
@@ -30,4 +30,36 @@ $fichesproduits = array (
       'srcImageProduit' = ''
       )
     );
+
+// création du pseudo code
+// boucle for pour mettre 12 produits sur le site
+// a chaque tour choisir une des cles du premier niveau au hasard
+// puis faire des echos qui suit le modele suivant de aterialize
+{
+    echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+}
+echo
+'<div class="row">
+   <div class="col s12 m7">
+     <div class="card">
+       <div class="card-image">
+         <img src="'.METTRE SOURCE DE LIMAGE.'">
+         <span class="card-title">'.METTRE TITRE.'</span>
+       </div>
+       <div class="card-content">
+         <p>'.METTRE DESCRIPTION.'</p>
+       </div>
+       <div class="card-action">
+         <a href="#">'.METTRE LIEN VERS PRODUIT.'</a>
+       </div>
+     </div>
+   </div>
+ </div>';
+
+// foreach($fichesproduits as $element)
+// {
+//     foreach ($element as $key => $value) {
+//       # code...
+//     }
+// }
 ?>
