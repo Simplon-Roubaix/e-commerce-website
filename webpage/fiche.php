4b8f5adce90cb82e8fd317cd403e8a39f260ae7b
farrradise <marzini.f@gmail.com>
2017-09-08 15:24:59 +0200

    start to code the real loop with real algo

--- webpage/fiche.php
+++ webpage/fiche.php
@@ -2,59 +2,69 @@
 // On crée notre array $fichesproduits
 $fichesproduits = array (
     '1' => array(
-      'titre' => 'titre1',
-      'description_courte' => 'BLABLABLA',
-      'resume' => '',
-      'prix' => '2',
-      'srcImageProduit' = ''
+      'titre' => 'Samsung Galaxy S7',
+      'description' => '',
+      'resume' => 'Memorabile mirum quodam quodam agi est mirum quodam plebem videre ardore plebem infuso curulium videre.',
+      'prix' => '100',
+      'srcImageProduit' = 'img/samsunggalaxys7.jpg',
+      'classDansLink' = 'a renseigner par taslim'
       ),
     '2' => array(
-      'titre' => 'titre',
-      'description_courte' => 'blablablou',
-      'resume' => '',
+      'titre' => 'Iphone',
+      'description' => 'blablablou',
+      'resume' => 'Praesertim senatus plaga libidines praeter plaga se plaga libidines senatus praeter hac inusta hanc inusta.',
       'prix' => '5',
-      'srcImageProduit' = ''
+      'srcImageProduit' = 'img/iphone.png',
+      'classDansLink' = 'a renseigner par taslim'
       ),
     '3' => array(
-      'titre' => 'titre',
-      'description_courte' => 'blablablou',
-      'resume' => '',
+      'titre' => 'Samsung Galaxy S5',
+      'description' => '',
+      'resume' => 'Orientem quidem ad sane iactitabant feris latrociniis inrequietis inopinis in adfligebat adfligebat capiti et hac.',
       'prix' => '5',
-      'srcImageProduit' = ''
+      'srcImageProduit' = 'img/samsunggalaxys5.jpg',
+      'classDansLink' = 'a renseigner par taslim'
       ),
     '4' => array(
-      'titre' => 'titre',
-      'description_courte' => 'blablablou',
-      'resume' => '',
+      'titre' => 'Nokia 3210',
+      'description' => 'blablablou',
+      'resume' => 'Possumus si afferat intellegam sententiae quidem oratio quot habeat me non et non re plane.',
       'prix' => '5',
-      'srcImageProduit' = ''
+      'srcImageProduit' = 'img/Nokia3210.jpg',
+      'classDansLink' = 'a renseigner par taslim'
       )
     );
 
-// création du pseudo code
-// boucle for pour mettre 12 produits sur le site
+// OK boucle for pour mettre 12 produits sur le site
 // a chaque tour choisir une des cles du premier niveau au hasard
-// puis faire des echos qui suit le modele suivant de aterialize
-{
-    echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
-}
-echo
-'<div class="row">
-   <div class="col s12 m7">
-     <div class="card">
-       <div class="card-image">
-         <img src="'.METTRE SOURCE DE LIMAGE.'">
-         <span class="card-title">'.METTRE TITRE.'</span>
-       </div>
-       <div class="card-content">
-         <p>'.METTRE DESCRIPTION.'</p>
-       </div>
-       <div class="card-action">
-         <a href="#">'.METTRE LIEN VERS PRODUIT.'</a>
+// puis faire des echos qui suit le modele suivant de materialize
+// {
+//     echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+// }
+
+for (i = 0; i < 12 ; i++ ) {
+
+  
+
+  echo
+  '<div class="row">
+     <div class="col s12 m7">
+       <div class="card">
+         <div class="card-image">
+           <img src="' . $['srcImageProduit'] . '">
+           <span class="card-title">'. $['titre']. '</span>
+         </div>
+         <div class="card-content">
+           <p>' . $['resume']. '</p>
+         </div>
+         <div class="card-action">
+           <a href="#" class=".$['classDansLink'].">En savoir plus</a>
+         </div>
        </div>
      </div>
-   </div>
- </div>';
+   </div>';
+
+}
 
 // foreach($fichesproduits as $element)
 // {
