097baf658ce156812fce002c30399c78a71072fd
taslimazzi <azzi.taslim@gmail.com>
2017-09-18 15:03:59 +0200

    css footer / add page article.php and article_.php

--- webpage/footer.php
+++ webpage/footer.php
@@ -23,7 +23,9 @@
             </div>
           </div>
         </footer>
-<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
 <script src="js/plugins.js"></script>
 <script src="js/main.js"></script>
