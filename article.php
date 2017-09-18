216fd2902e9a14bdb5533d599ece3175bdba0a1b
taslimazzi <azzi.taslim@gmail.com>
2017-09-18 19:22:32 +0200

    css and modal for article.php finish

--- article.php
+++ article.php
@@ -1,20 +1,41 @@
 <?php include("webpage/header.php");?>
 <?php if (isset($_GET['submission']) && $_GET['submission'] == 'ok' ): ?>
-  <?php echo "chocolat"?>
+
 <?php endif; ?>
 
-
-<form id="formArticle" action="article_.php" method="post" enctype="multipart/form-data">
-  <input  name="titre" type="text" class="form-control" placeholder="titre" required>
-  <textarea name="description" class="form-control" placeholder="description" required></textarea>
-  <input  name="resume" type="text" class="form-control" placeholder="resume" required>
-  <input  name="prix" type="number" class="form-control" placeholder="prix" required>
-  <input  name="url" type="text" class="form-control" placeholder="url"required>
-  <input type="file" name="image" class="form-control" >
-  <button class="btn waves-effect waves-light">Submit
-    <i class="material-icons right">send</i>
-  </button>
-</form>
+<div class="container-fluid valign-wrapper" id="AddRemove">
+ <div class="row" id="rowAddRemove">
+  <button class="waves-effect waves-light btn modal-trigger red lighten-2 col s3 " data-target="modal1">Add</button>
+  <button class="waves-effect waves-light btn modal-trigger red lighten-2 col s3 " data-target="modal2">remove</button>
+ </div>   
+</div>
 
 
+  
+  <div id="modal1" class="modal">
+    <div class="modal-content">
+      <div class="row">
+          <form id="formArticle" action="article_.php" method="post" enctype="multipart/form-data">
+            <input  name="titre" type="text" class="form-control" placeholder="titre" required>
+            <textarea name="description" class="form-control" placeholder="description" required></textarea>
+            <input  name="resume" type="text" class="form-control" placeholder="resume" required>
+            <input  name="prix" type="number" class="form-control" placeholder="prix" required>
+            <input  name="url" type="text" class="form-control" placeholder="url"required>
+            <input type="file" name="image" class="form-control" >
+            <button class="btn waves-effect waves-light red lighten-2">Submit
+              <i class="material-icons right">send</i>
+            </button>
+          </form>         
+      </div>
+    </div>
+    <div class="modal-footer">
+      <a class=" modal-action modal-close waves-effect waves-green btn-flat">Close</a>
+    </div>
+  </div>
+  
+  
+  
+    
+    
+    
 <?php include("webpage/footer.php");?>
